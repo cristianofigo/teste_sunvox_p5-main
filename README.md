f8bd7c5cddefa4faa590141ff9fb2ddb432cb187
Cristiano Figo <figocris@gmail.com>
2023-08-11 16:19:35 -0300

    começo

---
+++ README.md
@@ -0,0 +1,2 @@
+# testesunvox
+Created with CodeSandbox
---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    canvas = createCanvas(windowWidth, 100);
+    canvas.position(0,130);
+    //canvas.position(0, 0);
+    //canvas.style('zdex', '-1');
+    
+    colorMode(HSB, 360);
+    
+    textAlign(CENTER);
+    background("#7FFFD400");
+    samp.autostart = true;
+               
+    } 
+  
+  function draw(){
+    fill(200, 100, 0, 50);
+    let rnd = random(50);
+    ellipse(mouseX, mouseY, rnd, rnd);
+    background("#7FFFD403");
+    
+  }
