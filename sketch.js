54367233807121b99dd256b13902ff4f8170380f
Cristiano Figo <figocris@gmail.com>
2023-09-04 09:28:25 -0300

    sincroniza numero de linha com void draw()

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 //var nms = sv_get_number_of_modules;
+//let linha = sv_get_current_line(0);
 function setup() {
     canvas = createCanvas(windowWidth, 100);
     canvas.position(200,0);
@@ -21,7 +22,15 @@ function setup() {
     textSize(16);
     text(sv_get_song_name(0), 150, 30);
     text("BPM:" + sv_get_song_bpm( 0 ), 100, 50);
+    text(sv_get_current_line(0), 100, 70);
     //sv_get_song_bpm( 0 )
     background("#7FFFD403");
-    
+
+    if (sv_get_current_line(0) >= 20 && sv_get_current_line(0) < 100 ){
+        background (200, 0, 0);
+    }
+    else{
+        background("#7FFFD403");
+
+    }
   }
